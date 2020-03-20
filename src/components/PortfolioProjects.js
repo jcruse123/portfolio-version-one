7fc4b8432b1c780af709aca2017d2752ca2574dd
Joshua Cruse <jcruse123@gmail.com>
2020-03-20 17:48:28 -0600

    redesigned project section

--- src/components/PortfolioProjects.js
+++ src/components/PortfolioProjects.js
@@ -2,18 +2,26 @@ import React from 'react';
 import { Parallax } from 'react-parallax'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
-import randomWeather from '../img/random-weather.jpg'
-import nasaImage from '../img/nasa-image.jpg'
-import aperture from '../img/aperture.jpg'
-import openTicket from '../img/open-ticket.jpg'
-import portfolio from '../img/portfolio-bg.jpg'
+import Project from './Project'
+import randomWeather from '../img/projects/random-weather.jpg'
+import randomWeatherDetail from '../img/projects/random-weather-detail.jpg'
+import randomWeatherCode from '../img/projects/random-weather-code.jpg'
+import nasaImage from '../img/projects/nasa-image.jpg'
+import nasaImageDetail from '../img/projects/nasa-image-detail.jpg'
+import nasaImageCode from '../img/projects/nasa-image-code.jpg'
+import aperture from '../img/projects/aperture.jpg'
+import apertureDetail from '../img/projects/aperture-detail.jpg'
+import apertureCode from '../img/projects/aperture-code.jpg'
+import openTicket from '../img/projects/open-ticket.jpg'
+import openTicketDetail from '../img/projects/open-ticket-detail.jpg'
+import openTicketCode from '../img/projects/open-ticket-code.jpg'
+import portfolio from '../img/projects/portfolio.jpg'
+import portfolioDetail from '../img/projects/portfolio-detail.jpg'
+import portfolioCode from '../img/projects/portfolio-code.jpg'
 
 
 const PortfolioProjects = () => {
-  // Show projects as Bootstrap Cards in grid system
+  // Show row of Project components
   return (
     <div style={{backgroundColor: 'black'}}>
       <Parallax
@@ -29,89 +37,62 @@ const PortfolioProjects = () => {
             <hr className="pb-3" id="project-hr" />
             <Row>
 
-              <Col xs={12} sm={6} lg={4} className="mb-4">
-                <Card className=" h-100" data-aos="fade-in">
-                  <Card.Img variant="top" src={randomWeather} />
-                  <hr className="mt-0 pt-0 mb-0 pb-0 card-hr" />
-                  <Card.Body className="d-flex flex-column">
-                    <h3>Random Weather</h3>
-                    <hr className="project-title-hr mb-2 mt-1"/>
-                    <Card.Text>
-                      This application displays a number of locations along with weather data from those locations.
-                      Click on a map marker to display detailed weather information.
-                    </Card.Text>
-                    <Button className="mt-auto" href="https://random-weather.herokuapp.com/" block size="sm" variant="success">Live Version</Button>
-                    <Button href="https://github.com/jcruse123/random-weather" block size="sm" variant="primary">GitHub</Button>
-                  </Card.Body>
-                </Card>
-              </Col>
+              <Project
+                image = {randomWeather}
+                imageDetail = {randomWeatherDetail}
+                imageCode = {randomWeatherCode}
+                title = "Random Weather"
+                description = "This application displays a number of locations along with weather data from those locations. Click on a map marker to display detailed weather information."
+                github = "https://github.com/jcruse123/random-weather/"
+                link = "https://random-weather.herokuapp.com/"
+                modal = "This application consists of two parts. It uses a back end server built using Node.js and Express that makes use of two external APIs to get weather data about a specific number of random locations in the world. This data is then used by the front end to display the data on an interactive map. The front end is built using React and Mapbox."
+                modalTwo= "The objective for this project was to gain experience integrating data from external APIs into an interactive front end."
+              />
 
-              <Col xs={12} sm={6} lg={4} className="mb-4">
-                <Card className=" h-100" data-aos="fade-in">
-                  <Card.Img variant="top" src={nasaImage} />
-                  <hr className="mt-0 pt-0 mb-0 pb-0 card-hr" />
-                  <Card.Body className="d-flex flex-column">
-                    <h3>NASA Image Library</h3>
-                    <hr className="project-title-hr mb-2 mt-1"/>
-                    <Card.Text>
-                      NASA Image Library uses an API to view images in a collection provided by NASA.
-                      Click on an image to learn more about space exploration.
-                    </Card.Text>
-                    <Button className="mt-auto" href="https://jcruse123.github.io/nasa-image/" block size="sm" variant="success">Live Version</Button>
-                    <Button href="https://github.com/jcruse123/nasa-image" block size="sm" variant="primary">GitHub</Button>
-                  </Card.Body>
-                </Card>
-              </Col>
+              <Project
+                image = {nasaImage}
+                imageDetail = {nasaImageDetail}
+                imageCode = {nasaImageCode}
+                title = "NASA Image Library"
+                description = "NASA Image Library uses an API to view images in a collection provided by NASA. Click on an image to learn more about space exploration."
+                github = "https://github.com/jcruse123/nasa-image/"
+                link = "https://jcruse123.github.io/nasa-image/"
+                modal = "This is a front end application made using React and Node.js. On the first page, a list of the most popular images from NASA's image library is displayed using an endpoint on their free API. The search function uses a different endpoint that returns search results when provided with search terms. "
+                modalTwo = "The objective for this project was to gain experience integrating data from external APIs into an interactive front end."
+              />
 
-              <Col xs={12} sm={6} lg={4} className="mb-4">
-                <Card className=" h-100" data-aos="fade-in">
-                  <Card.Img variant="top" src={openTicket} />
-                  <hr className="mt-0 pt-0 mb-0 pb-0 card-hr" />
-                  <Card.Body className="d-flex flex-column">
-                    <h3>Open Ticket</h3>
-                    <hr className="project-title-hr mb-2 mt-1"/>
-                    <Card.Text>
-                      Open Ticket is an easy to use issue tracker powered by React.
-                      Create a new ticket or track your progress on an existing ticket using journal entries.
-                    </Card.Text>
-                    <Button className="mt-auto" href="https://open-ticket-complete.herokuapp.com/" block size="sm" variant="success">Live Version</Button>
-                    <Button href="https://github.com/jcruse123/open-ticket-complete" block size="sm" variant="primary">GitHub</Button>
-                  </Card.Body>
-                </Card>
-              </Col>
+              <Project
+                image = {openTicket}
+                imageDetail = {openTicketDetail}
+                imageCode = {openTicketCode}
+                title = "Open Ticket"
+                description = "Open Ticket is an easy to use issue tracker powered by React. Create a new ticket or track your progress on an existing ticket using journal entries."
+                github = "https://github.com/jcruse123/open-ticket/"
+                link = "https://open-ticket-complete.herokuapp.com"
+                modal = "Open Ticket is an easy to use issue tracker powered by React. Create a new ticket or track your progress on an existing ticket using journal entries."
+              />
 
-              <Col xs={12} sm={6} lg={4} className="mb-4">
-                <Card className="h-100" data-aos="fade-in">
-                  <Card.Img variant="top" src={aperture} />
-                  <hr className="mt-0 pt-0 mb-0 pb-0 card-hr" />
-                  <Card.Body className="d-flex flex-column">
-                    <h3>Aperture</h3>
-                    <hr className="project-title-hr mb-2 mt-1"/>
-                    <Card.Text>
-                      Aperture is a place for photography enthusiasts to post their best photos.
-                      Sign up to post your own photo and leave a five star rating on your favorite picture.
-                    </Card.Text>
-                    <Button className="mt-auto" href="https://aperture-photo.herokuapp.com/photos" block size="sm" variant="success">Live Version</Button>
-                    <Button href="https://github.com/jcruse123/aperture-photo" block size="sm" variant="primary">GitHub</Button>
-                  </Card.Body>
-                </Card>
-              </Col>
+              <Project
+                image = {aperture}
+                imageDetail = {apertureDetail}
+                imageCode = {apertureCode}
+                title = "Aperture"
+                description = "Aperture is a place for photography enthusiasts to post their best photos. Sign up to post your own photo and leave a five star rating on your favorite picture."
+                github = "https://github.com/jcruse123/aperture-photo"
+                link = "https://aperture-photo.herokuapp.com/photos/"
+                modal = "Aperture is a place for photography enthusiasts to post their best photos. Sign up to post your own photo and leave a five star rating on your favorite picture."
+              />
 
-              <Col xs={12} sm={6} lg={4} className="mb-4">
-                <Card className="h-100" data-aos="fade-in">
-                  <Card.Img variant="top" src={portfolio} />
-                  <hr className="mt-0 pt-0 mb-0 pb-0 card-hr" />
-                  <Card.Body className="d-flex flex-column">
-                    <h3>Developer Portfolio</h3>
-                    <hr className="project-title-hr mb-2 mt-1"/>
-                    <Card.Text>
-                      Custom web page to display my projects and allow anyone to easily send me a message.
-                      No link provided - you're already here!
-                    </Card.Text>
-                    <Button className="mt-auto" href="https://github.com/jcruse123/portfolio-version-one" block size="sm" variant="primary">GitHub</Button>
-                  </Card.Body>
-                </Card>
-              </Col>
+              <Project
+                image = {portfolio}
+                imageDetail = {portfolioDetail}
+                imageCode = {portfolioCode}
+                title = "Developer Portfolio"
+                description = "Custom web page to display my projects and allow anyone to easily send me a message."
+                github = "https://github.com/jcruse123/portfolio-version-one/"
+                link = "https://jcruse123.github.io/portfolio-version-one/"
+                modal = "Custom web page to display my projects and allow anyone to easily send me a message."
+              />
 
             </Row>
           </Container>

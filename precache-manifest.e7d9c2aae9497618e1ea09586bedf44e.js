291da4716c2beeeb389a879a09e6ab30d01ea941
Joshua Cruse <jcruse123@gmail.com>
2020-03-20 18:19:47 -0600

    Updates

---
+++ precache-manifest.e7d9c2aae9497618e1ea09586bedf44e.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f420e3bba3c25a2a7dc56ff4ec6b2d3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c27113f428d3b1d66847",
+    "url": "/static/css/2.cbf16948.chunk.css"
+  },
+  {
+    "revision": "622395096e0d88a7f5db",
+    "url": "/static/css/main.7ddb19d8.chunk.css"
+  },
+  {
+    "revision": "c27113f428d3b1d66847",
+    "url": "/static/js/2.517e50cc.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.517e50cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "622395096e0d88a7f5db",
+    "url": "/static/js/main.d77a3420.chunk.js"
+  },
+  {
+    "revision": "62ce67ad23a5026d0696",
+    "url": "/static/js/runtime-main.69181c57.js"
+  },
+  {
+    "revision": "099d4a637f4faa64795e993e6e4bcf49",
+    "url": "/static/media/aperture-code.099d4a63.jpg"
+  },
+  {
+    "revision": "6b164977ad5bc033181e810166e75e3f",
+    "url": "/static/media/aperture-detail.6b164977.jpg"
+  },
+  {
+    "revision": "28bc44658040eb060684e635f26e7a6e",
+    "url": "/static/media/aperture.28bc4465.jpg"
+  },
+  {
+    "revision": "c315b6194bcf6a8a7f679b6910dc1694",
+    "url": "/static/media/bg.c315b619.jpg"
+  },
+  {
+    "revision": "694954db2ecd5e7d3412ba95ed4d833b",
+    "url": "/static/media/email.694954db.svg"
+  },
+  {
+    "revision": "0c00727816c1a986616c49675e44916f",
+    "url": "/static/media/headshot.0c007278.jpg"
+  },
+  {
+    "revision": "b5d7892ecf3d1789c9f5fe80874a2ae5",
+    "url": "/static/media/home.b5d7892e.svg"
+  },
+  {
+    "revision": "e8b695a1efec48d67e0b65fac2009c14",
+    "url": "/static/media/list.e8b695a1.svg"
+  },
+  {
+    "revision": "b527907d7fa7d61665c23ba81b9ebf64",
+    "url": "/static/media/nasa-image-code.b527907d.jpg"
+  },
+  {
+    "revision": "8e1d61ac37d9784f55994be3ca929c99",
+    "url": "/static/media/nasa-image-detail.8e1d61ac.jpg"
+  },
+  {
+    "revision": "01265069b0ec07a539d75f4eb6b4e4bf",
+    "url": "/static/media/nasa-image.01265069.jpg"
+  },
+  {
+    "revision": "27cb61b9ff693ac2c58f45816b00e651",
+    "url": "/static/media/open-ticket-code.27cb61b9.jpg"
+  },
+  {
+    "revision": "220e16cca0fce4174ca3f5712f339235",
+    "url": "/static/media/open-ticket-detail.220e16cc.jpg"
+  },
+  {
+    "revision": "700695836830a01fe2c544ed4f8e8764",
+    "url": "/static/media/open-ticket.70069583.jpg"
+  },
+  {
+    "revision": "253b58a1bfd87f839da8e87a4dbd235e",
+    "url": "/static/media/person.253b58a1.svg"
+  },
+  {
+    "revision": "c86b896e83d81c82e598e840544b3fc4",
+    "url": "/static/media/portfolio-code.c86b896e.jpg"
+  },
+  {
+    "revision": "ee2e8e6230f946ac98c1bc16e6bbad31",
+    "url": "/static/media/portfolio-detail.ee2e8e62.jpg"
+  },
+  {
+    "revision": "2da6e2911f578863a727039134f9ccdc",
+    "url": "/static/media/portfolio.2da6e291.jpg"
+  },
+  {
+    "revision": "91cc680aa12199f721b54bf0a6baef32",
+    "url": "/static/media/projects-bg.91cc680a.jpg"
+  },
+  {
+    "revision": "1b8fa5cce966f979772e33fa82da3478",
+    "url": "/static/media/random-weather-code.1b8fa5cc.jpg"
+  },
+  {
+    "revision": "188b6c3569f4a0c60026723c2bf3084b",
+    "url": "/static/media/random-weather-detail.188b6c35.jpg"
+  },
+  {
+    "revision": "29840df0bb1313bdc8cb63d4256aec64",
+    "url": "/static/media/random-weather.29840df0.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-version-one/precache-manifest.e3265876e9beb649180bbf1b0c88b287.js"
+  "/precache-manifest.e7d9c2aae9497618e1ea09586bedf44e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-version-one/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
